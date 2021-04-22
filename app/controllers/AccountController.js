20da5aeb15f4362d5bda7dce3ca260e6136894f7
DiegoRuiz92 <diegoruizc.05@gmail.com>
2021-04-22 11:33:01 -0500

    changes in AccountController

--- app/controllers/AccountController.js
+++ app/controllers/AccountController.js
@@ -49,3 +49,55 @@ AccountController.consignAccount = async (req, res, next) => {
         next(error);
     }
 }
+
+AccountController.withdraw = async (req, res, next) => {
+    
+    const params = req.params;
+    
+    const body = req.body;
+
+    try {
+        await AccountService.withdraw(param.id, body.amount)
+        res.send({message: 'operation succeeded'})
+        //-------------------------------
+    } catch (error) {
+        console.log({error});
+        res.status(500).send({error: error.message}).end();
+        next(error);
+    }
+}
+
+AccountController.transfer = async (req, res, next) => {
+
+    const body = req.body;
+
+    try {
+        await AccountService.transfer(body, body.amount)
+        res.send({message: 'transfer succeeded'})
+        //-------------------------------
+    } catch (error) {
+        console.log({error});
+        res.status(500).send({error: error.message}).end();
+        next(error);
+    }
+}
+
+AccountController.delete = async (req, res, next) => {
+    //extrayendo los PathParams de la peticion
+    const params = req.params;
+
+    try {
+        //se supone que el id llega asi /customer/:id (aca no es con {} sino con :)
+        //await (ya que el metodo es async) para esperar que termine.
+        await AccountService.delete(params.id)
+
+        //enviando respuesta al cliente (postman por ejemplo)
+        res.send({message: 'account deleted'})
+        //_____________________________________
+    } catch (error) { //manejando las excepciones...
+        console.log({error});
+        //retornando la cuenta(postman por ejemplo) el error..
+        res.status(500).send({error: error.message}).end();
+        next(error);
+    }
+}
--- app/controllers/routes.js
+++ app/controllers/routes.js
@@ -7,8 +7,11 @@ const router = express.Router();
 router.delete('/customers/:id', CustomerController.delete);
 router.put('/customers/:id', CustomerController.edit);
 router.get('/customers/:id/accounts', AccountController.listAccountsByCustomer);
+
 router.post('/accounts', AccountController.createAccount);
-router.put('/customers/:id/withdraw', CustomerController.edit);
-router.put('/customers/:id/consign', CustomerController.edit);
+router.put('/accounts/:id/withdraw', AccountController.edit);
+router.put('/accounts/:id/consign', AccountController.consignAccount);
+router.put('/accounts/:id/transfer', AccountController.withdraw);
+router.delete('/accounts/:id', AccountController.delete);
 
 module.exports = router;
